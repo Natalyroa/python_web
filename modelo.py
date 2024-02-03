c84614fe637ccad0033fc8c4c074f3fcb223068d
Nataly Roa <nataly.roa0513@gmail.com>
2024-02-02 19:46:30 -0500

    ejercicio_clase_5

---
+++ modelo.py
@@ -0,0 +1,7 @@
+def obtener_liros():
+    # Simulacion de una bd de libros
+    return[
+        {"id": 1, "titulo": "cien años de soledad", "autor": "Gabriel Garcias"},
+        {"id": 2, "titulo":"la sombra del viento", "autor": "carlos ruiz"},
+        {"id": 3, "titulo": "El nombre del viento", "autor":"Patrick Rothfuss" }
+    ]
---
+++ vista.py
@@ -0,0 +1,6 @@
+from modelo import obtener_liros
+
+def mostrar_libros():
+    libros =obtener_liros()
+    for libro in libros:
+        print(f"ID: {libro['id']}, titulo:{libro['titulo']}, autor:{libro['autor']}")
